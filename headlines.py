48f0b410f4ad535022d86b5eeb2c3aa1f88c6668
Nicolas Ramirez <nram.vel94@gmail.com>
2023-10-09 15:38:20 -0500

    with Jinja templates

--- headlines.py
+++ headlines.py
@@ -1,5 +1,6 @@
 import feedparser
 from flask import Flask
+from flask import render_template
 
 
 app = Flask(__name__)
@@ -26,16 +27,26 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 def getNews(publication="bbc"):
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
-    return """<html>
-                <body>
-                    <h1> Headlines </h1>
-                    <b>{0}</br>
-                    <i>{1}</br>
-                    <p>{2}</br>
-                </body>
-            </html>""".format(first_article.get("title"), 
-                              first_article.get("published"), 
-                              first_article.get("summary"))
+    return render_template("home.html", articles=feed['entries'])
+    # Cleaner way of populating dynamically
+    # return render_template("home.html", article=first_article)
+    # One way to dynamically populate your tmeplates
+    # return render_template("home.html",
+    #                        title=first_article.get("title"),
+    #                        published=first_article.get("published"),
+    #                        summary=first_article.get("summary")
+    #                        )
+    # You can return html in your return statements
+    # return """<html>
+    #             <body>
+    #                 <h1> Headlines </h1>
+    #                 <b>{0}</br>
+    #                 <i>{1}</br>
+    #                 <p>{2}</br>
+    #             </body>
+    #         </html>""".format(first_article.get("title"), 
+    #                           first_article.get("published"), 
+    #                           first_article.get("summary"))
 
 
 if __name__ == '__main__':
